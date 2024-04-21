86952f41bfe9a2285fa3952b231c9bd9ac4737ce
sardelkaaa <adeliya64775@gmail.com>
2024-04-21 03:17:46 +0300

    + pages

---
+++ lib/pages/all_tasks.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class AllTasks extends StatelessWidget {
+  const AllTasks({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
