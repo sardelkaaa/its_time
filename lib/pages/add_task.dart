67595153bbf05035701a9cb0cf752a9341dad277
Alexander Sokolov <sokol2006312@mail.ru>
2024-04-23 22:09:55 +0500

    add task 60%

--- lib/pages/add_task.dart
+++ lib/pages/add_task.dart
@@ -8,37 +8,100 @@ class AddTask extends StatelessWidget {
       backgroundColor: Color(0xFF0A1128), // Цвет фона страницы
       appBar: AppBar(
         backgroundColor: Color(0xFF0A1128), // Цвет фона шапки
-        actions: <Widget>[
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new),
+          iconSize: MediaQuery.of(context).size.width * 0.1,
+          color: Color(0xFFC6E9F3),
+          onPressed: () {
+            Navigator.pushNamed(context, '/'); // Вернуться на главную страницу
+          },
+        ), // Кнопка вернуться на главный экран
+
+        title: Center(
+            child: Text('Новое задание', // Добавляем текст "Новое задание" в качестве заголовка
+              style: TextStyle(
+                color: Color(0xFFC6E9F3),
+                fontSize: 21,
+                fontWeight: FontWeight.w500
+                ),
+              ),
+            ),
+
+        actions: [
           IconButton(
             icon: const Icon(Icons.notifications),
-            iconSize: MediaQuery.of(context).size.width * 0.1,// Устанавляем размер иконки пропорционально ширине экрана
+            iconSize: MediaQuery.of(context).size.width * 0.1,
             color: Color(0xFFC6E9F3),
             onPressed: () {
-              Navigator.pushNamed(context,
-                  '/notifications'); // Действие при нажатии на значок уведомлений
+              Navigator.pushNamed(context, '/notifications'); // Действие при нажатии на значок уведомлений
             },
-          ),
+          ), // Кнопка уведомлений
         ],
       ),
 
-      body: Column(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width * 0.95,
-            height: MediaQuery.of(context).size.height * 0.735,
-            margin: const EdgeInsets.all(10.0),
-            padding: const EdgeInsets.all(10.0),
-            decoration: BoxDecoration(
-              gradient: const LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [Color(0xFF1282A2), Color(0xFF034078)],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width * 0.95,
+              height: MediaQuery.of(context).size.height * 0.73, // Уменьшаем высоту контейнера
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [Color(0xFF1282A2), Color(0xFF034078)],
+                ),
+                borderRadius: BorderRadius.circular(35.0),
+                color: Colors.grey[200],
               ),
-              borderRadius: BorderRadius.circular(35.0),
-              color: Colors.grey[200],
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Название',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Дата',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Время',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: 'Описание',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 230), // Добавляем отступ
+
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Отправить'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFF1282A2),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
 
       bottomNavigationBar: BottomAppBar(
