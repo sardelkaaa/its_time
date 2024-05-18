823845879868693d4817cb707df210c6e722770d
sardelkaaa <adeliya64775@gmail.com>
2024-05-18 18:09:59 +0500

    notifications without background mode

--- lib/services/TaskServices.dart
+++ lib/services/TaskServices.dart
@@ -1,9 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:its_time/pages/editTask.dart';
-
-
 
 class TaskServices extends ChangeNotifier{
 
